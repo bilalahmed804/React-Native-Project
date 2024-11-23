57428aac6add601b97ef7b3835e0b5e703bd0c66
Bilal Ahmed <ba3801534@gmail.com>
2024-11-23 13:55:07 +0500

    React-Native-Bykea-clone

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,7 +6,9 @@ import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { StyleSheet, Text, View } from 'react-native';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -28,12 +30,38 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+   <View style={styles.conatiner}>
+   <View style={styles.header}>
+   <MaterialIcons name="menu" size={24} color="black" />
+   <Text style={styles.headerTaxt}>BYKEA</Text>
+   <MaterialIcons name="add-call" size={24} color="black" />
+
+   {/* Banner image  */}
+     <View style={styles.bannerImageView}></View>
+   </View>
+   </View>
+  ); 
 }
+
+const styles = StyleSheet.create({
+  conatiner:{
+    flex:1,
+  },
+  headerTaxt:{
+    fontWeight:"bold",
+    letterSpacing:3,
+    fontSize:21,
+  },
+  header:{
+    height:50,
+    borderBottomColor: "#ccc",
+    borderBottomWidth:1,
+    flexDirection:"row",
+    alignItems:"center",
+    justifyContent:"space-between", 
+    paddingHorizontal:16,
+  },
+  bannerImageView:{
+    flex:1,
+  }
+})
